ae4206cb9944af0d085d595796bb1501e4c77f9b
darkrush <kidfatenight@gmail.com>
2018-12-08 21:35:18 +0800

    clear code

--- DDPG/ddpg.py
+++ DDPG/ddpg.py
@@ -3,108 +3,63 @@ import copy
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.optim import Adam,SGD,RMSprop
-from obs_norm import Run_Normalizer
+from torch.optim import Adam
+from arguments import Singleton_arger
+
+from model import Actor,Critic
 from memory import Memory
-from sgld import SGLD
 
 class DDPG(object):
-    def __init__(self, actor_lr, critic_lr, lr_decay,
-                 l2_critic, batch_size, discount, tau,
-                 action_noise, noise_decay,
-                 parameter_noise, SGLD_noise,
-                 SGLD_mode, num_pseudo_batches, 
-                 with_cuda):
-        self.actor_lr = actor_lr
-        self.critic_lr = critic_lr
-        self.lr_decay = lr_decay
-        self.l2_critic = l2_critic
-        self.batch_size = batch_size
-        self.discount = discount
-        self.tau = tau
-
-        self.SGLD_noise = SGLD_noise
-        self.action_noise = action_noise
-        self.noise_decay = noise_decay
-        self.parameter_noise = parameter_noise
-        self.SGLD_mode = SGLD_mode
-        self.adapt_pseudo_batches = num_pseudo_batches is 0
-        self.num_pseudo_batches = num_pseudo_batches
+    def __init__(self):
+        agent_args = Singleton_arger()['agent']
+        self.actor_lr = agent_args['actor_lr']
+        self.critic_lr = agent_args['critic_lr']
+        self.lr_decay = agent_args['lr_decay']
+        self.l2_critic = agent_args['l2_critic']
+        self.batch_size = agent_args['batch_size']
+        self.discount = agent_args['discount']
+        self.tau = agent_args['tau']
+        self.with_cuda = agent_args['with_cuda']
+        self.buffer_size = int(agent_args['buffer_size'])
         
-        exploration_method = (self.action_noise is not None) + (self.parameter_noise is not None) + (self.SGLD_mode is not 0)
-        assert exploration_method <=1
-        
-        self.with_cuda = with_cuda
-        
-    def setup(self, actor, critic, memory, obs_norm = None ):
+    def setup(self, nb_states, nb_actions):
         self.lr_coef = 1
-        self.noise_coef = 1
+        
+        model_args = Singleton_arger()['model']
+        actor  = Actor (nb_states, nb_actions, hidden1 = model_args['hidden1'], hidden2 = model_args['hidden2'] , layer_norm = model_args['layer_norm'])
+        critic = Critic(nb_states, nb_actions,hidden1 = model_args['hidden1'], hidden2 = model_args['hidden2'] , layer_norm = model_args['layer_norm'])
+        
         self.actor         = copy.deepcopy(actor)
         self.actor_target  = copy.deepcopy(actor)
         self.critic        = copy.deepcopy(critic)
         self.critic_target = copy.deepcopy(critic)
         
-        self.noise_actor = None
-        self.measure_actor = None
-        if self.SGLD_mode is not 0:
-            self.noise_actor   = copy.deepcopy(actor)
-        if self.parameter_noise is not None:
-            self.noise_actor   = copy.deepcopy(actor)
-            self.measure_actor = copy.deepcopy(actor)
-        
-        self.obs_norm = obs_norm
+        self.memory = Memory(self.buffer_size, (nb_actions,), (nb_states,), self.with_cuda)
         
         if self.with_cuda:
-            for net in (self.actor, self.actor_target, self.noise_actor, self.measure_actor, self.critic, self.critic_target, self.obs_norm):
+            for net in (self.actor, self.actor_target, self.critic, self.critic_target):
                 if net is not None:
                     net.cuda()
-                    
-            
-        if (self.SGLD_mode == 1)or(self.SGLD_mode == 3):
-            self.actor_optim  = SGLD(self.actor.parameters(),
-                                     lr=self.actor_lr,
-                                     num_pseudo_batches = self.num_pseudo_batches,
-                                     num_burn_in_steps = 1000)
-        else :
-            self.actor_optim  = Adam(self.actor.parameters(), lr=self.actor_lr)
         
-        if (self.SGLD_mode == 2)or(self.SGLD_mode == 3):
-            p_groups = [{'params': [param,],
-                         'noise_switch': self.SGLD_noise and (True if ('LN' not in name) else False),
-                         'weight_decay': self.l2_critic if ('weight' in name) and ('LN' not in name) else 0
-                        } for name,param in self.critic.named_parameters() ]
-            self.critic_optim  = SGLD(params = p_groups,
-                                      lr = self.critic_lr,
-                                      num_pseudo_batches = self.num_pseudo_batches,
-                                      num_burn_in_steps = 1000,
-                                      weight_decay = self.l2_critic)
-        else:
-            p_groups = [{'params': [param,],
-                         'weight_decay': self.l2_critic if ('weight' in name) and ('LN' not in name) else 0
-                        } for name,param in self.critic.named_parameters() ]
-            self.critic_optim  = Adam(params = p_groups, lr=self.critic_lr, weight_decay = self.l2_critic)
+        p_groups = [{'params': [param,],
+                     'weight_decay': self.l2_critic if ('weight' in name) and ('LN' not in name) else 0
+                    } for name,param in self.critic.named_parameters() ]
+        self.critic_optim  = Adam(params = p_groups, lr=self.critic_lr, weight_decay = self.l2_critic)
+        self.actor_optim  = Adam(self.actor.parameters(), lr=self.actor_lr)
         
-        self.memory = memory
-                        
     def reset_noise(self):
-        if self.action_noise is not None:
-            self.action_noise.reset()
-        if self.parameter_noise is not None:
-            for target_param, param in zip(self.noise_actor.parameters(), self.actor.named_parameters()):
-                name, param = param
-                if 'LN' not in name:
-                    target_param.data.copy_(param.data + torch.normal(mean=torch.zeros_like(param),std=torch.full_like(param,self.parameter_noise.current_stddev)))
-                else:
-                    target_param.data.copy_(param.data)
-        if self.SGLD_mode is not 0:
-            self.noise_actor.load_state_dict(copy.deepcopy(self.actor.state_dict()))
-            
+        pass
+        
+    def before_epoch(self):
+        pass
+    
+    def before_cycle(self):
+        pass
+        
     def store_transition(self, s_t, a_t, r_t, s_t1, done_t):
         s_t = torch.tensor(s_t,dtype = torch.float32,requires_grad = False)
         if self.with_cuda:
             s_t = s_t.cuda()
-        if self.obs_norm is not None:
-            self.obs_norm.observe(s_t)
         self.memory.append(s_t, a_t, r_t, s_t1, done_t)
 
     def update_critic(self, batch = None, pass_batch = False):
@@ -114,9 +69,6 @@ class DDPG(object):
         assert batch is not None
         tensor_obs0 = batch['obs0']
         tensor_obs1 = batch['obs1']
-        if self.obs_norm is not None:
-            tensor_obs0 = self.obs_norm(tensor_obs0)
-            tensor_obs1 = self.obs_norm(tensor_obs1)
         # Prepare for the target q batch
         with torch.no_grad():
             next_q_values = self.critic_target([
@@ -161,57 +113,25 @@ class DDPG(object):
     def update_critic_target(self,soft_update = True):
         for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
             target_param.data.copy_(target_param.data * (1.0 - self.tau) + param.data * self.tau \
-                                    if soft_update \
-                                    else target_data)
+                                    if soft_update else param.data)
 
     def update_actor_target(self,soft_update = True):
         for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
             target_param.data.copy_(target_param.data * (1.0 - self.tau) + param.data * self.tau \
-                                    if soft_update \
-                                    else target_data)
-                
-    def update_num_pseudo_batches(self):
-        if not self.adapt_pseudo_batches:
-            return
-        for opt in (self.actor_optim,self.critic_optim):
-            if isinstance(opt,SGLD):
-                for group in opt.param_groups:
-                    group['num_pseudo_batches'] = self.memory.nb_entries
-
+                                    if soft_update else param.data)
+                                    
     def apply_lr_decay(self):
         if self.lr_decay > 0:
             self.lr_coef = self.lr_decay*self.lr_coef/(self.lr_coef+self.lr_decay)
-            for opt in (self.actor_optim,self.critic_optim):
+            for (opt,base_lr) in ((self.actor_optim,self.actor_lr),(self.critic_optim,self.critic_lr)):
                 for group in opt.param_groups:
-                    group['lr'] = self.actor_lr * self.lr_coef
+                    group['lr'] = base_lr * self.lr_coef
             
-    def apply_noise_decay(self):
-        if self.noise_decay > 0:
-            self.noise_coef = self.noise_decay*self.noise_coef/(self.noise_coef+self.noise_decay)
-            
-    def adapt_param_noise(self):
-        if self.parameter_noise is None:
-            return
-        for target_param, param in zip(self.measure_actor.parameters(), self.actor.named_parameters()):
-            name, param = param
-            if 'LN' not in name:
-                target_param.data.copy_(param.data + torch.normal(mean=torch.zeros_like(param),std=torch.full_like(param,self.parameter_noise.current_stddev)))
-            else:
-                target_param.data.copy_(param.data)
-        batch = self.memory.sample(self.batch_size)
-        tensor_obs0 = batch['obs0']
-        with torch.no_grad():
-            distance = torch.mean(torch.sqrt(torch.sum((self.actor(tensor_obs0)- self.measure_actor(tensor_obs0))**2,1)))
-        self.parameter_noise.adapt(distance)
-    
     def calc_last_error(self):
         # Sample batch
         batch = self.memory.sample_last(self.batch_size)
         tensor_obs0 = batch['obs0']
         tensor_obs1 = batch['obs1']
-        if self.obs_norm is not None:
-            tensor_obs0 = self.obs_norm(tensor_obs0)
-            tensor_obs1 = self.obs_norm(tensor_obs1)
         # Prepare for the target q batch
         with torch.no_grad():
             next_q_values = self.critic_target([
@@ -223,45 +143,24 @@ class DDPG(object):
             value_loss = nn.functional.mse_loss(q_batch, target_q_batch)
         return value_loss.item()
         
-    def select_action(self, s_t, if_noise = True):
+    def select_action(self, s_t, apply_noise):
         s_t = torch.tensor(s_t,dtype = torch.float32,requires_grad = False)
         if self.with_cuda:
             s_t = s_t.cuda()
-        if self.obs_norm is not None:
-            s_t = self.obs_norm(s_t)
         with torch.no_grad():
-            if not if_noise :
-                action = self.actor(s_t).cpu().numpy().squeeze(0)
-            else:
-                if (self.SGLD_mode is not 0):
-                    action = self.noise_actor(s_t).cpu().numpy().squeeze(0)
-                elif (self.parameter_noise is not None):
-                    action = self.noise_actor(s_t).cpu().numpy().squeeze(0)
-                elif (self.action_noise is not None):
-                    action = self.actor(s_t).cpu().numpy().squeeze(0)
-                    action += max(self.noise_coef, 0)*self.action_noise()
-                else:
-                    action = self.actor(s_t).cpu().numpy().squeeze(0)
+            action = self.actor(s_t).cpu().numpy().squeeze(0)
         action = np.clip(action, -1., 1.)
         return action
         
     def load_weights(self, output): 
         self.actor  = torch.load('{}/actor.pkl'.format(output) )
         self.critic = torch.load('{}/critic.pkl'.format(output))
-        if self.obs_norm is not None:
-            self.obs_norm  = torch.load('{}/obs_norm.pkl'.format(output) )
             
     def save_model(self, output):
         torch.save(self.actor ,'{}/actor.pkl'.format(output) )
         torch.save(self.critic,'{}/critic.pkl'.format(output))
-        if self.obs_norm is not None:
-            torch.save(self.obs_norm ,'{}/obs_norm.pkl'.format(output) )
             
     def get_actor_buffer(self):
         actor_buffer = io.BytesIO()
         torch.save(self.actor, actor_buffer)
-        obs_norm_buffer = None
-        if self.obs_norm is not None:
-            obs_norm_buffer = io.BytesIO()
-            torch.save(self.obs_norm, obs_norm_buffer)
-        return (actor_buffer,obs_norm_buffer)
+        return actor_buffer
--- DDPG/evaluator.py
+++ DDPG/evaluator.py
@@ -13,16 +13,14 @@ class Evaluator(object):
     def __init__(self):
         self.env = None
         self.actor = None
-        self.obs_norm = None
+
         self.visualize = False
         
-    def setup(self, env_name, logger, obs_norm = False, num_episodes = 10, max_episode_length=1000, model_dir = None,
+    def setup(self, env_name, logger,  num_episodes = 10,  model_dir = None,
               multi_process = True, visualize = False, rand_seed = -1):
         self.env_name = env_name
         self.logger = logger
-        self.if_obs_norm = obs_norm
         self.num_episodes = num_episodes
-        self.max_episode_length = max_episode_length
         self.model_dir = model_dir
         self.multi_process = multi_process
         self.visualize = visualize
@@ -53,17 +51,12 @@ class Evaluator(object):
                 self.run_eval(item)
             
     def load_from_buffer(self, buffer):
-        if self.if_obs_norm:
-            buffer, norm_buffer = buffer
-            self.obs_norm = torch.load(norm_buffer)
-        self.actor = torch.load(actor_buffer)
+        self.actor = torch.load(buffer)
         
     def laod_from_file(self,model_dir = None):
         if model_dir is None:
             model_dir = self.model_dir
         assert model_dir is not None
-        if self.if_obs_norm:
-            self.obs_norm = torch.load(os.path.join(model_dir,'obs_norm.pkl'))
         self.actor = torch.load(os.path.join(model_dir,'actor.pkl'))
         
     def run_eval(self,total_cycle):
@@ -80,8 +73,6 @@ class Evaluator(object):
             done = False
             while not done:
                 obs = torch.tensor([observation],dtype = torch.float32,requires_grad = False).cuda()
-                if self.obs_norm is not None:
-                    obs = self.obs_norm(obs)
                 with torch.no_grad():
                     action = self.actor(obs).cpu().numpy().squeeze(0)
                 action = np.clip(action, -1., 1.)
@@ -134,10 +125,7 @@ if __name__ == "__main__":
     parser.add_argument('--logdir', default=None, type=str, help='result output dir')
     parser.add_argument('--env', default=None, type=str, help='open-ai gym environment')
     parser.add_argument('--model-dir', default=None, type=str, help='actor for evaluation')
-    parser.add_argument('--max-episode-length', default=1000, type=int, help='max step number for pisode')
-    parser.add_argument('--num-episodes', default=10, type=int, help='max step number for pisode')
-    parser.add_argument('--obs-norm', dest='obs_norm', action='store_true',help='enable observation normalization')
-    parser.set_defaults(obs_norm=False)
+    parser.add_argument('--num-episodes', default=10, type=int, help='number of episodes')
     parser.add_argument('--visualize', dest='visualize', action='store_true',help='enable render in evaluation progress')
     parser.set_defaults(visualize=False)
     
@@ -145,7 +133,6 @@ if __name__ == "__main__":
     if args.logdir is not None:
         with open(args.logdir,'rb') as f:
             exp_args = pickle.load(f)
-            args.obs_norm = exp_args.obs_norm
             args.env = exp_args.env
             args.model_dir = exp_args.result_dir
             
@@ -154,9 +141,7 @@ if __name__ == "__main__":
     
     Singleton_evaluator.setup(env_name = args.env,
                               logger = None,
-                              obs_norm = args.obs_norm,
                               num_episodes = 10,
-                              max_episode_length=args.max_episode_length,
                               model_dir = args.model_dir,
                               multi_process = False,
                               visualize = args.visualize,
